cf7cec27d778cbc9924740e602a18b0c2c144d0e
Chris Freeman <cc.freem@yahoo.com>
2018-12-13 10:14:20 -0800

    update to most recent chatframe and switch to test agent webhooks

--- public/app/src/App.js
+++ public/app/src/App.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react'
 import Chatframe from '@cambriasolutions/chatframe'
 
 const dfWebhookOptions = {
-  eventUrl:
-    'https://us-central1-dhcs-demo-chat.cloudfunctions.net/eventRequest',
-  textUrl: 'https://us-central1-dhcs-demo-chat.cloudfunctions.net/textRequest',
+  eventUrl: 'https://us-central1-webchat-core.cloudfunctions.net/eventRequest',
+  textUrl: 'https://us-central1-webchat-core.cloudfunctions.net/textRequest',
 }
 
 class App extends Component {
