f0a454be454c36a185420ef65542faa075c491ce
Chris Freeman <cc.freem@yahoo.com>
2019-03-09 17:11:04 -0800

    update chatframe and dependencies

--- public/app/src/App.js
+++ public/app/src/App.js
@@ -2,21 +2,34 @@ import React, { Component } from 'react'
 import Chatframe from '@cambriasolutions/chatframe'
 
 const dfWebhookOptions = {
-  eventUrl: 'https://us-central1-webchat-core.cloudfunctions.net/eventRequest',
-  textUrl: 'https://us-central1-webchat-core.cloudfunctions.net/textRequest',
+  eventUrl: 'https://us-central1-mdhs-csa-dev.cloudfunctions.net/eventRequest',
+  textUrl: 'https://us-central1-mdhs-csa-dev.cloudfunctions.net/textRequest',
+}
+
+const policyText =
+  'Please do not enter any personally identifiable information such as SSN, Date of Birth or last name.'
+
+export const mapConfig = {
+  googleMapsKey: process.env.REACT_APP_GOOGLE_MAPS_KEY,
+  centerCoordinates: {
+    lat: 32.777025,
+    lng: -89.543724,
+  },
 }
 
 class App extends Component {
   render() {
     return (
       <Chatframe
-        primaryColor="#3bafbf"
-        secondaryColor="#3bafbf"
-        title="Gen"
-        client="Dialogflow"
+        primaryColor='#3bafbf'
+        secondaryColor='#3bafbf'
+        title='Cambria'
+        client='Dialogflow'
         clientOptions={dfWebhookOptions}
         fullscreen={false}
         initialActive={false}
+        policyText={policyText}
+        mapConfig={mapConfig}
       />
     )
   }
