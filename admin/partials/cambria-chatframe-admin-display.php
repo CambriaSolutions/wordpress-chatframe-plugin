b340f9232250d9cc04e1698350a28e73cc8484bf
Jade Tran <jtran@cambriasolutions.com>
2019-03-21 13:52:20 -0700

    added admin page and submenu

---
+++ admin/partials/cambria-chatframe-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       cambriasolutions.com
+ * @since      1.0.0
+ *
+ * @package    Cambria_Chatframe
+ * @subpackage Cambria_Chatframe/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
--- includes/class-cambria-chatframe.php
+++ includes/class-cambria-chatframe.php
@@ -75,6 +75,8 @@ class Cambria_Chatframe {
 
 		$this->load_dependencies();
 		$this->define_public_hooks();
+		$this->define_admin_hooks();
+
 	}
 
 	/**
@@ -105,6 +107,8 @@ class Cambria_Chatframe {
 		 * side of the site.
 		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-cambria-chatframe-public.php';
+		
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-cambria-chatframe-admin.php';
 
 		$this->loader = new Cambria_Chatframe_Loader();
 
@@ -126,6 +130,14 @@ class Cambria_Chatframe {
 
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
+	}
+
+	private function define_admin_hooks() {
+		$plugin_admin = new Cambria_Chatframe_Admin( $this->get_plugin_name(), $this->get_version() );
+
+		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
+		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
+		$this->loader->add_action( 'admin_menu', $plugin_admin, 'my_plugin_menu' );
 	}
 
 	/**
