ef3f855d5fda25f4bce878cece7483f7aaf2f3d8
Jade Tran <jtran@cambriasolutions.com>
2019-05-31 11:04:20 -0700

    only show chatframe in child support page

--- white-listed-pages.php
+++ white-listed-pages.php
@@ -2,4 +2,4 @@
 
 // Urls containing any of theses elements will allow our chatframe
 // to appear on the page
- $acceptedPathArray = array("child-support", "page-3");
+ $acceptedPathArray = array("child-support");
