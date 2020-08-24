5cf9335e294d28fc01941cc82eb49397eaa67033
Brian Havice <haviceb@infinity-software.com>
2020-08-24 14:32:52 -0400

    Adding workforce development

--- white-listed-pages.php
+++ white-listed-pages.php
@@ -2,4 +2,4 @@
 
 // Urls containing any of theses elements will allow our chatframe
 // to appear on the page
- $acceptedPathArray = array("/", "/child-support", "/child-support/", "/economic-assistance", "/economic-assistance/");
+ $acceptedPathArray = array("/", "/child-support", "/child-support/", "/economic-assistance", "/economic-assistance/", "/workforce-development", "/workforce-development/");
