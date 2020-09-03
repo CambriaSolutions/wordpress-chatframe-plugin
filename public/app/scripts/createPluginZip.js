099fdbaf11ac352d7e4d023a3113442b71cbd2bb
Brian Havice <haviceb@infinity-software.com>
2020-09-03 15:15:44 -0400

    Updated for generating plugin zips as well as the version update

---
+++ public/app/scripts/createPluginZip.js
@@ -0,0 +1,48 @@
+
+var fs = require('fs');
+var archiver = require('archiver');
+
+var output = fs.createWriteStream('../../../GenBotPlugin.zip');
+var archive = archiver('zip', {
+    zlib: { level: 9 } // Sets the compression level.
+});
+
+output.on('close', function () {
+    console.log(archive.pointer() + ' total bytes');
+    console.log('archiver has been finalized and the output file descriptor has closed.');
+});
+
+output.on('end', function () {
+    console.log('Data has been drained');
+});
+
+archive.on('warning', function (err) {
+    if (err.code === 'ENOENT') {
+        // log warning
+    } else {
+        // throw error
+        throw err;
+    }
+});
+
+// good practice to catch this error explicitly
+archive.on('error', function (err) {
+    throw err;
+});
+
+// pipe archive data to the file
+archive.pipe(output);
+
+// append a file from stream
+archive.file('../../../cambria-chatframe.php')
+archive.file('../../../index.php')
+archive.file('../../../uninstall.php')
+archive.file('../../../white-listed-pages.php')
+archive.directory('../../partials', 'public/partials')
+archive.file('../../class-cambria-chatframe-public.php', { name: 'public/class-cambria-chatframe-public.php' })
+archive.file('../../index.php', { name: 'public/index.php' })
+archive.directory('../build', 'public/app/build')
+archive.directory('../../../admin', 'admin')
+archive.directory('../../../includes', 'includes')
+
+archive.finalize();
